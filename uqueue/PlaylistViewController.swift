266064bd1057de334dc3643fde7931d94af4a89f
sfotsch <sfotsch@nd.edu>
2015-10-13 20:09:03 -0400

    add playlist table view

---
+++ uqueue/PlaylistViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlaylistViewController.swift
+//  uqueue
+//
+//  Created by Shawn Fotsch on 10/13/15.
+//  Copyright © 2015 John Rocha and Shawn Fotsch. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
