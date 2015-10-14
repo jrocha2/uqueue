9adca529a70af24614c3d5e44701a662439fe4ad
sfotsch <sfotsch@nd.edu>
2015-10-13 23:19:27 -0400

    make playlists accessible

--- uqueue/PlaylistViewController.swift
+++ uqueue/PlaylistViewController.swift
@@ -7,29 +7,48 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class PlaylistViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tableData = MPMediaQuery.playlistsQuery()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
 
-        // Do any additional setup after loading the view.
-    }
+    self.tableView.reloadData()
+    
+}
+
+
+func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+    return self.tableData.collections!.count
+    
+}
+
+func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell     {
+    
+    let cell: UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+    
+    let playlist: MPMediaPlaylist = self.tableData.collections![indexPath.row] as! MPMediaPlaylist
+    let playlistName = playlist.valueForProperty(MPMediaPlaylistPropertyName) as! NSString
+    cell.textLabel?.text = playlistName as String
+    
+    return cell
+}
+
+func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+    print("Row \(indexPath.row) selected")
+}
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
